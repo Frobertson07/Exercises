773b3b26b10d6118c2d57c1fbf91d453097d8399
Frobertson07 <92533857+Frobertson07@users.noreply.github.com>
2021-10-28 14:56:39 +0100

    adds basic variable program exercise

---
+++ Variables.cpp
@@ -0,0 +1,16 @@
+//Declare a variable with data type integer. Initialise with a value of 8.
+//Declare a variable with data type integer. Initialise with a value of 10.
+//Declare a third variable of data type integer that adds the previous variables together.
+//Print the value of the sum to the console.   
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int varOne = 8;
+  int varTwo = 10;
+  int varThree = varOne + varTwo;
+  cout << varThree << endl;
+
+  return 0;
+}
